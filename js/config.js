c955a40445aa79681dc212b4b7097371e9dc8a8d
Abhilash <abhilash.m@multunus.com>
2012-11-30 17:53:22 +0530

    Integrated with actual data

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
 googleSpreadSheet = {
-    key: '0AsDS1JguvF2vdFRaNXF4RmhxNU9GSXBRbzJIRl80MXc',
+//    key: '0AsDS1JguvF2vdFRaNXF4RmhxNU9GSXBRbzJIRl80MXc',
+    key: '0Atvw0HKzxaMcdG5XUEk2T3B6YmlqQjFXV192c2NHcnc',
     daysSheets: ['1', '2', '3'],
     speakersSheets: ['4']
 };
