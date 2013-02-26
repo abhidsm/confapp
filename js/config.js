a31d46b64e215d57778683a99376387de53d51a9
Abhilash M A <abhidsm@gmail.com>
2013-02-26 22:46:47 +0530

    Removed the 2nd and 3rd day info

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 googleSpreadSheet = {
 //    key: '0AsDS1JguvF2vdFRaNXF4RmhxNU9GSXBRbzJIRl80MXc',
     key: '0Atvw0HKzxaMcdG5XUEk2T3B6YmlqQjFXV192c2NHcnc',
-    daysSheets: ['1', '2', '3'],
+    daysSheets: ['1'],
     speakersSheets: ['4'],
     infoSheet: ['5']
 };
