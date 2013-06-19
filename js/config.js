122b4c9dad1a0130ebc17ab19633842854c94615
Ernest S. <ernest.sankeshwar@gmail.com>
2013-06-19 19:11:10 +0530

    reverted the doc

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
 googleSpreadSheet = {
 //    key: '0AsDS1JguvF2vdFRaNXF4RmhxNU9GSXBRbzJIRl80MXc',
-//    key: '0Atvw0HKzxaMcdG5XUEk2T3B6YmlqQjFXV192c2NHcnc', spreadsheet which contains improper lean startup conf data
-    key: '0ApUPwJdQvqT_dENoVXJ5ZnlVME1qeVc1bzl1ZnpkS0E',
+   key: '0Atvw0HKzxaMcdG5XUEk2T3B6YmlqQjFXV192c2NHcnc',
     daysSheets: ['1'],
     speakersSheets: ['4'],
     infoSheet: ['5']
